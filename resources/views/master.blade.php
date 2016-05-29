ae278c21cfe0f66665f560c1a5f58aa7432a810c
quang_home <nnduyquang@gmail.com>
2016-05-29 18:52:23 +0700

    Fix menutop cho giao diện mobile

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>Laravel</title>
     {!! Html::style('public/css/bootstrap.min.css') !!} 
-    {!! Html::style('public/css/bootstrap-theme.min.css') !!} 
     {!! Html::style('public/css/menutop.css') !!}
-    {!! Html::script('public/js/jquery-1.12.4.min.js') !!}
-    {!! Html::script('public/js/bootstrap.min.js') !!}
-    {!! Html::script('public/js/menutop.js') !!}
 </head>
-<body>
-    <div class="main-wrapper">
-        <div class="main-wrapper-header">
-           {{--Nhúng header menu, logo,..--}}
-           @include('menu.menutop')
 
-    </div>
+<body>
+    {{-- Gắn menu, header--}}
+    @include('menu.menutop')
+    
+    {{-- Gắn main contain--}}
     <div class="main-contain container">
         @yield('main-contain')
     </div>
-</div>
+
+    {!! Html::script('public/js/jquery-1.12.4.min.js') !!}
+    <script>window.jQuery || document.write('<script src="http://localhost:8080/smartlinks_v2/public/js/jquery-1.12.4.min.js"><\/script>')</script>
+    {!! Html::script('public/js/bootstrap.min.js') !!}
 
 </body>
 </html>
